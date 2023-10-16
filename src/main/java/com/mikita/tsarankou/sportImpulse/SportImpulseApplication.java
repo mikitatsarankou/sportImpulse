0a1180a750d0e63d69fa13c13bc959c9394c96a3
nekit <tsarenkovn@gmail.com>
2023-10-16 23:58:43 +0200

    Initial message

---
+++ src/main/java/com/mikita/tsarankou/sportImpulse/SportImpulseApplication.java
@@ -0,0 +1,13 @@
+package com.mikita.tsarankou.sportImpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportImpulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportImpulseApplication.class, args);
+    }
+
+}
